085e86f322f72443a95a7167e57b240799eccd2b
chartataglance <mariadass@msn.com>
2022-02-09 18:52:06 +0000

    intcommit

---
+++ theme.js
@@ -0,0 +1,33 @@
+
+const theme = localStorage.getItem('theme')
+if(theme ==null){
+   setTheme('light')
+}else {
+   setTheme(theme)
+}
+  
+const themeDots = document.getElementsByClassName("theme-dot");
+
+for (var i = 0; themeDots.length > i; i++) {
+  themeDots[i].addEventListener("click", function(){
+    let mode = this.dataset.mode
+    setTheme (mode)
+    
+  }) 
+  
+}
+
+function setTheme (mode){
+   if(mode == 'light'){
+      document.getElementById('theme-style').href = 'default.css'
+   }
+   if(mode == 'blue'){
+      document.getElementById('theme-style').href = 'blue.css'
+   }
+   if(mode == 'green'){
+      document.getElementById('theme-style').href = 'green.css'
+   }
+
+   localStorage.setItem('theme', mode)
+   
+}
